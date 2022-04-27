5bfc706aa17163d6b9d0d6c520ffe39494c80e59
Dante Basso <dcbasso@gmail.com>
2022-04-27 11:29:20 -0300

    Add route, controller, repostiory and service

--- RocketSeat/src/server.ts
+++ RocketSeat/src/server.ts
@@ -1,17 +1,17 @@
 import "reflect-metadata";
 import express from "express";
+import { router } from "./routes";
 
 import "./database";
 
 
 const app = express();
 
-app.get("/status", (request, response) => {
-  return response.send("Running");
-});
+app.use(express.json);
+app.use(router);
 
-// app.post("/test-post", (request, response) => {
-//   return response.send("Olá NLW método POST");
+// app.get("/status", (request, response) => {
+//   return response.send("Running");
 // });
 
 app.listen(3000, () => console.log("Server is running"));
