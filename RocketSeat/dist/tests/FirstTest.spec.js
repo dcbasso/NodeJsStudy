adc2e82a0ff759cde6080ad7a20a7eb1e06ad572
Dante Basso <dcbasso@gmail.com>
2022-04-26 21:40:41 -0300

    Update gitignore, removing not necessary files to commit

--- RocketSeat/dist/tests/FirstTest.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = require("../models/User");
-test('it should run', () => {
-    const user = new User_1.User();
-    user.name = 'Dante';
-    user.email = 'dcbasso@gmail.com';
-    expect(user.name).toEqual('Dante');
-});
